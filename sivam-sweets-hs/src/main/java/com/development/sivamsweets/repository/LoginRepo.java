d9b2b2811e765c9a1bd59f214154467c78cf3d28
Harinath S <160600940+Harinath-S-342@users.noreply.github.com>
2024-02-27 21:03:11 +0530

    Delete sivam-sweets-hs directory

--- sivam-sweets-hs/src/main/java/com/development/sivamsweets/repository/LoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.development.sivamsweets.repository;
-
-import com.development.sivamsweets.entity.Login;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginRepo extends JpaRepository<Login,Integer>{
-}
